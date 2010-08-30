0cd4df1219d8105f67b33d2917db34ee30c887c6
Craig Ambrose <craig@craigambrose.com>
2010-08-30 20:37:56 +1200

    upgraded to rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtamaiRails::Application.config.secret_token = '230f1a9a84bce075c308956b38d091281f59f6d193aa3b79f7ea30a783015306dc7d1313d62eae21d777b2c3273197c1a9d02cd2bbb16789faaf423fbb014ad6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AtamaiRails::Application.config.session_store :cookie_store, :key => '_atamai_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# AtamaiRails::Application.config.session_store :active_record_store
