0762f4018f6257c14fb1cb3fb0659fbb79c72fc4
Craig Ambrose <craig@craigambrose.com>
2011-01-22 22:11:09 +1300

    added more deploy stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :scm, :git
 set :rails_env, "production"
 set :deploy_to, "/home/atamai/public_html/atamai_rails"
 set :user, 'atamai'
+set :use_sudo, false
 
 namespace :deploy do
   task :start do ; end
@@ -17,4 +18,17 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+end
+
+after "deploy:update_code" do
+  link_from_shared_to_current('config')
+end
+after "deploy", "deploy:cleanup"
+after "deploy:migrations", "deploy:cleanup"
+
+
+def link_from_shared_to_current(path, dest_path = path)
+  src_path = "#{shared_path}/#{path}"
+  dst_path = "#{release_path}/#{dest_path}"
+  run "for f in `ls #{src_path}/` ; do ln -nsf #{src_path}/$f #{dst_path}/$f ; done"
 end
