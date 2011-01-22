4396c30d5c6f3f943e4bf5e8b0a6e221af768fe4
Craig Ambrose <craig@craigambrose.com>
2011-01-23 12:38:24 +1300

    Added root page for now

---
+++ app/controllers/patterns_controller.rb
@@ -0,0 +1,5 @@
+class PatternsController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ AtamaiRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "patterns#index"
 
   # See how all your routes lay out with "rake routes"
 
