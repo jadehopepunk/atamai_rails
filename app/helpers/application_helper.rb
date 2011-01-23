2d62a27895537136c5228419aefcec2973aac41b
Craig Ambrose <craig@craigambrose.com>
2011-01-23 19:51:03 +1300

    added a facebook like button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def facebook_like(url = nil)
+    url ||= request.url
+    content_tag(:iframe, nil, 
+      :src => "http://www.facebook.com/plugins/like.php?href=#{URI.encode(url)}&amp;layout=standard&amp;show_faces=true&amp;width=450&amp;action=like&amp;colorscheme=light&amp;height=80",
+      :scrolling => "no",
+      :frameborder => "0",
+      :style => "border:none; overflow:hidden; width:450px; height:80px;",
+      :allowTransparency => "true",
+      :class => 'facebook facebook_like'
+    )
+  end
 end
