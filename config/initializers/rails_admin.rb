59a480e89700e7383e4c9fe935d272b39d4dda7c
Craig Ambrose <craig@craigambrose.com>
2011-01-23 19:03:23 +1300

    Added ckeditor

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -6,7 +6,9 @@ RailsAdmin.config do |config|
     edit do
       field :title, :string
       [:summary, :the_problem, :how_bad_is_it, :the_solutions, :what_can_i_do].each do |name|
-        field name, :text
+        field name, :text do
+          ckeditor true
+        end
       end
     end
   end
---
+++ public/javascripts/ckeditor/config.js
@@ -0,0 +1,25 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+  config.toolbar = 'Full';
+  config.toolbar_Full = [
+      ['Source'],
+      ['Paste','PasteText','PasteFromWord', 'SpellChecker', 'Scayt'],
+      ['Undo','Redo','RemoveFormat'],
+      ['Bold','Italic','Underline','Strike','Subscript','Superscript'],
+      '/',
+      ['NumberedList','BulletedList','Outdent','Indent','Blockquote','CreateDiv'],
+      ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+      ['Link','Unlink','Anchor'],
+      ['Image','Table','HorizontalRule','SpecialChar'],
+      ['Format','ShowBlocks']
+  ];
+  
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
