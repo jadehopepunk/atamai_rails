f3975c2119d6c8a65b57d64f538f77e818b8609d
Craig Ambrose <craig@craigambrose.com>
2011-02-26 19:56:13 +1300

    Added challenge logos and index

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -1,5 +1,10 @@
 module ChallengesHelper
-  def challenge_logo
-    image_tag 'challenges/large/peak_oil_main.jpg', :size => '200x150'
+  def challenge_logo_link(challenge, image_options = {})
+    link_to challenge_logo(challenge, image_options), challenge_path(challenge), :title => challenge.short_title, :alt => challenge.short_title
+  end
+  
+  def challenge_logo(challenge, options = {})
+    defaults = {:size => '200x150'}
+    image_tag "challenges/large/#{challenge.to_param.gsub('-', '_')}_main.jpg", defaults.merge(options)
   end
 end
